dc04c8af2bd2ed64f120db7fd067d6026929a306
lokovalski <kovalskilola@gmail.com>
2024-12-11 12:26:56 -0500

    hi

--- ner_processing.py
+++ ner_processing.py
@@ -1,9 +1,10 @@
-from span_marker import SpanMarkerModel
+from transformers import AutoTokenizer
 import numpy as np
 from tqdm import tqdm
+import torch
 import os
 import json
-import time
+from span_marker import SpanMarkerModel
 
 def sanity_check(inputs):
     '''
@@ -37,8 +38,6 @@ def sanity_check(inputs):
     f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
 
     return precision, recall, f1
-    
-
 
 def identify_named_entities(inputs, language, split):
     """
@@ -49,30 +48,34 @@ def identify_named_entities(inputs, language, split):
         dict(str: Tensors): source, source_entities_labeled, source_entities_predicted,
                         target, target_entities_labeled, target_entities_predicted
     """
-    source = np.array([line['source'] for line in inputs], dtype = object)
-    target = np.array([line['target'] for line in inputs], dtype = object)
+    source = np.array([line['source'] for line in inputs], dtype=object)
+    target = np.array([line['target'] for line in inputs], dtype=object)
     lstents = [obs['entities'] for obs in inputs]
-    keys = [ent for key in lstents for ent in key]
 
-    source_entities_labeled = np.array([item[key]['en'] for item in lstents for key in item], dtype = object)
-    target_entities_labeled = np.array([item[key][language] for item in lstents for key in item], dtype = object)
+    source_entities_labeled = np.array([item[key]['en'] for item in lstents for key in item], dtype=object)
+    target_entities_labeled = np.array([item[key][language] for item in lstents for key in item], dtype=object)
 
-    model = SpanMarkerModel.from_pretrained("tomaarsen/span-marker-mbert-base-multinerd", clean_up_tokenization_spaces = True)
+    model = SpanMarkerModel.from_pretrained("tomaarsen/span-marker-mbert-base-multinerd", clean_up_tokenization_spaces=True)
 
-    source_pred = np.array([model.predict(sentence) for sentence in source], dtype = object)
-    source_entities_predicted = np.array([[d['span'] for d in row] for row in source_pred], dtype = object)
-    target_pred = np.array([model.predict(sentence) for sentence in target], dtype = object)   
-    target_entities_predicted = np.array([[d['span'] for d in row] for row in target_pred], dtype = object)
+    source_pred = np.array([model.predict(sentence) for sentence in tqdm(source, desc="Processing source sentences")], dtype=object)
+    source_entities_predicted = np.array([[d['span'] for d in row] for row in source_pred], dtype=object)
+    target_pred = np.array([model.predict(sentence) for sentence in tqdm(target, desc="Processing target sentences")], dtype=object)
+    target_entities_predicted = np.array([[d['span'] for d in row] for row in target_pred], dtype=object)
 
     results = {"source": source, "source_entities_labeled": source_entities_labeled, "source_entities_predicted": source_entities_predicted,
-            "source_pred": source_pred, "target": target, "target_entities_labeled": target_entities_labeled, "target_entities_predicted":  target_entities_predicted,
-            "target_pred": target_pred}
+               "source_pred": source_pred, "target": target, "target_entities_labeled": target_entities_labeled,
+               "target_entities_predicted": target_entities_predicted, "target_pred": target_pred}
 
     # Directory to save model output to reduce repeated computation
     output_dir = 'data'
     os.makedirs(output_dir, exist_ok=True)
 
-    with open(os.path.join(output_dir, 'NERpredictions_{}.json'.format(split)), 'w') as resultfile:
-        json.dump(results, resultfile, indent=4)
+    with open(os.path.join(output_dir, 'NERpredictions_{}{}.json'.format(split,language)), 'w') as resultfile:
+        json.dump({"source": source.tolist(), "source_entities_labeled": source_entities_labeled.tolist(),
+                   "source_entities_predicted": source_entities_predicted.tolist(),
+                   "source_pred": source_pred.tolist(), "target": target.tolist(),
+                   "target_entities_labeled": target_entities_labeled.tolist(),
+                   "target_entities_predicted": target_entities_predicted.tolist(),
+                   "target_pred": target_pred.tolist()}, resultfile, indent=4)
 
     return results
